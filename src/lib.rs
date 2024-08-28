5b6b49a03d449e1cb57691e29b4b7ef89e7bcb40
cmdoret <cyril.mattheydoret@gmail.com>
2024-08-28 15:36:01 +0200

    feat: basic lib structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,54 @@
+use std::{io::BufRead, path::Path};
 
-pub fn list() {
+use oxigraph::store::Store;
+use oxigraph::{
+    io::GraphFormat,
+    model::GraphNameRef,
+};
+use skim::prelude::*;
+
+struct Term {
+    uri: String,
+    label: String,
+}
+
+impl SkimItem for Term {
+    fn text(&self) -> Cow<str> {
+        Cow::Borrowed(&self.label)
+    }
+
+    fn preview(&self, _context: PreviewContext) -> ItemPreview {
+        ItemPreview::Text(format!("{} ({})", self.label, self.uri))
+    }
+}
+
+pub fn filter_terms(query: String, terms: Vec<Term>) {
     println!("Hello, world!");
+}
+
+
+// Build in-memory kg, load all sources and query for uris and labels.
+fn query(readers: Vec<impl BufRead>) -> Vec<Term>{
+    let store = Store::new().unwrap();
+    readers.iter().for_each(|r| {
+        store.load_graph(
+            r,
+            GraphFormat::Turtle,
+            GraphNameRef::DefaultGraph,
+            None,
+        ).unwrap();
+    });
+    let results = store.query("
+        SELECT ?uri ?label 
+        WHERE { 
+            ?uri <http://www.w3.org/2000/01/rdf-schema#label> ?label 
+        }"
+    ).unwrap();
+    results
+        .iter()
+        .map(|r| Term {
+            uri: r.get("uri"), label: r.get("label")
+        })
+        .collect()
+        .into()
 }

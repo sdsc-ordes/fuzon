e414265ee6e8bd3b8cb33ec2bd35cf4fd7c7f08e
cmdoret <cyril.mattheydoret@gmail.com>
2024-08-29 02:21:16 +0200

    feat(cli): dummy end-to-end run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    fuzon::list();
+    let file = b"<http://example.com/subject> <http://www.w3.org/2000/01/rdf-schema#label> \"Hello World\" .";
+    
+    let all_codes = fuzon::query(vec![file.as_ref()]);
+    let _hits = fuzon::filter_terms("Hello".to_string(), all_codes);
 }

1f91c5693de6e0727adea70c034b0936fcb02340
cmdoret <cyril.mattheydoret@gmail.com>
2024-08-29 02:20:55 +0200

    feat: oxigraph query

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use core::fmt;
 use std::{io::BufRead, path::Path};
 
+use oxigraph::sparql::QueryResults;
 use oxigraph::store::Store;
 use oxigraph::{
     io::GraphFormat,
@@ -16,14 +18,21 @@ struct TermMatcher {
 impl TermMatcher{
 
     fn new(terms: Vec<Term>) -> Self {
-        let (tx, rx) = unbounded(); // <- this should take a vec
+        let (tx, rx) = bounded(terms.len()); // <- this should take a vec
         Self { terms, tx, rx }
     }
 }
 
-struct Term {
+#[derive(Debug)]
+pub struct Term {
     uri: String,
     label: String,
+}
+
+impl fmt::Display for Term {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ({})", self.label, self.uri)
+    }
 }
 
 impl SkimItem for Term {
@@ -36,33 +45,37 @@ impl SkimItem for Term {
     }
 }
 
-pub fn filter_terms(query: String, terms: Vec<Term>) {
-    println!("Hello, world!");
+pub fn filter_terms(query: String, terms: impl Iterator<Item = Term>) {
+    terms.filter(|t| t.label.contains(&query))
+        .for_each(|t| println!("{}", t));
 }
 
 
 // Build in-memory kg, load all sources and query for uris and labels.
-fn query(readers: Vec<impl BufRead>) -> Vec<Term>{
+pub fn query(readers: Vec<impl BufRead>) -> impl Iterator<Item = Term> {
     let store = Store::new().unwrap();
-    readers.iter().for_each(|r| {
+    // NOTE: May want to use bulk loader for better performances
+    for reader in readers {
         store.load_graph(
-            r,
+            reader,
             GraphFormat::Turtle,
             GraphNameRef::DefaultGraph,
             None,
         ).unwrap();
-    });
+    }
     let results = store.query("
         SELECT ?uri ?label 
         WHERE { 
             ?uri <http://www.w3.org/2000/01/rdf-schema#label> ?label 
         }"
     ).unwrap();
-    results
-        .iter()
-        .map(|r| Term {
-            uri: r.get("uri"), label: r.get("label")
-        })
-        .collect()
-        .into()
+    if let QueryResults::Solutions(sol) = results {
+        sol.map(|r| r.unwrap())
+            .map(|r| Term {
+                uri: r.get("uri").unwrap().to_string(),
+                label: r.get("label").unwrap().to_string(),
+            })
+    } else {
+        panic!("Unexpected");
+    }
 }

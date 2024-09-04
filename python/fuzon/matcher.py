00222ac7aa77b3dc2393c1c71b0e6a349aaec327
cmdoret <cyril.mattheydoret@gmail.com>
2024-09-04 14:05:05 +0200

    feat(py): build python API

---
+++ python/fuzon/matcher.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from fuzon import Term, score_terms
+
+@dataclass
+class TermMatcher:
+    terms: list[Term]
+    
+    def top(self, query: str, n: int=5) -> list[Term]:
+        return self.rank(query)[:n]
+
+    def rank(self, query: str) -> list[Term]:
+        scores = self.score(query)
+        ranks = [
+            i[0] for i in 
+            sorted(enumerate(scores), key=lambda x:x[1], reverse=True)
+        ]
+        return [self.terms[rank] for rank in ranks]
+
+    def score(self, query: str) -> list[float]:
+        return score_terms(self.terms, query)

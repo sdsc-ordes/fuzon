cc817ea45739ea6f5a3fb6654ed95178d8eafadf
cmdoret <cyril.mattheydoret@gmail.com>
2024-09-06 16:08:32 +0200

    chore: add nix-based tooling

---
+++ tools/nix/flake.nix
@@ -0,0 +1,144 @@
+{
+  description = "fuzon";
+
+  nixConfig = {
+    substituters = [
+      # Add here some other mirror if needed.
+      "https://cache.nixos.org/"
+    ];
+    extra-substituters = [
+      # Nix community's cache server
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgsStable.url = "github:nixos/nixpkgs/nixos-23.11";
+    # Also see the 'stable-packages' overlay at 'overlays/default.nix'.
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # The Rust overlay to include the latest toolchain.
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }: let
+    # This is string (without toString it would be a `path` which is put into the store)
+    rootDir = toString ./. + "../../..";
+    python = pkgs.python312;
+  in
+    flake-utils.lib.eachDefaultSystem
+    # Creates an attribute map `{ devShells.<system>.default = ...}`
+    # by calling this function:
+    (
+      system: let
+        overlays = [(import rust-overlay)];
+
+        # Import nixpkgs and load it into pkgs.
+        # Overlay the rust toolchain
+        lib = nixpkgs.lib;
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        # Set the rust toolchain from the `rust-toolchain.toml`.
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ../../rust-toolchain.toml;
+
+        # Things needed only at compile-time.
+        nativeBuildInputsBasic = with pkgs; [
+          maturin
+          findutils
+          coreutils
+          bash
+          zsh
+          curl
+          git
+          jq
+        ];
+
+        # Things needed only at compile-time.
+        nativeBuildInputsDev = with pkgs; [
+          rustToolchain
+          cargo-watch
+          just
+
+          skopeo
+          dasel
+        ];
+
+        benchInputs = with pkgs; [
+          hyperfine
+          heaptrack
+        ];
+
+        # Things needed at runtime.
+        buildInputs = [];
+
+        # The package of this CLI tool.
+        # The global version for tripsu.
+        # This is gonna get tooled later.
+        fuzon = (import ./pkgs/fuzon.nix) {
+          inherit rootDir rustToolchain pkgs lib;
+        };
+      in
+        with pkgs; rec {
+          devShells = {
+            default = mkShell {
+              inherit buildInputs;
+              nativeBuildInputs = nativeBuildInputsBasic ++ nativeBuildInputsDev;
+            };
+            bench = mkShell {
+              inherit buildInputs;
+              nativeBuildInputs = nativeBuildInputsBasic 
+                ++ nativeBuildInputsDev
+                ++ benchInputs;
+            };
+
+            ci = mkShell {
+              inherit buildInputs;
+              nativeBuildInputs = nativeBuildInputsBasic ++ nativeBuildInputsDev;
+
+              # Due to some weird handling of TMPDIR inside containers:
+              # https://github.com/NixOS/nix/issues/8355
+              # We have to reset the TMPDIR to make `nix build` work inside
+              # a development shell.
+              # Without `nix develop` it works.
+              shellHook = "unset TMPDIR";
+            };
+          };
+
+          packages = {
+            tripsu = fuzon;
+
+            images = {
+              ci = (import ./images/ci.nix) {
+                inherit pkgs;
+                devShellDrv = devShells.default;
+              };
+
+              fuzon = (import ./images/fuzon.nix) {
+                inherit pkgs fuzon;
+              };
+            };
+          };
+        }
+    );
+}
---
+++ tools/nix/pkgs/fuzon.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  lib,
+  rustToolchain,
+  rootDir,
+  ...
+}: let
+  rustPlatform = pkgs.makeRustPlatform {
+    cargo = rustToolchain;
+    rustc = rustToolchain;
+  };
+
+  cargoFile = /. + rootDir + "fuzon/Cargo.toml";
+  lockFile = /. + rootDir + "fuzon/Cargo.lock";
+in
+  rustPlatform.buildRustPackage {
+    name = "fuzon";
+    src = /. + rootDir + "fuzon/";
+
+    version = (lib.importTOML cargoFile).package.version;
+
+    cargoLock = {
+      inherit lockFile;
+    };
+
+    meta = {
+      description = "A CLI tool to fuzzy search ontology terms by their labels.";
+      homepage = "https://github.com/sdsc-ordes/fuzon";
+      license = lib.licenses.asl20;
+      maintainers = ["gabyx" "cmdoret"];
+    };
+  }

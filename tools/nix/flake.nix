f6efb8a2dfa88e5c17654429d34c72f196ebf529
cmdoret <cyril.mattheydoret@gmail.com>
2025-05-23 13:31:29 +0200

    chore(nix): update maturin

--- tools/nix/flake.nix
+++ tools/nix/flake.nix
@@ -21,7 +21,7 @@
 
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
-    nixpkgsStable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgsStable.url = "github:nixos/nixpkgs/nixos-24.11";
     # Also see the 'stable-packages' overlay at 'overlays/default.nix'.
 
     flake-utils.url = "github:numtide/flake-utils";
